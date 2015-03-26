70ef2754c595914fc1a6c4167b5d3381c23812e8
mythseur <prunierbenjamin@gmail.com>
2015-03-26 22:56:55 +0100

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace TeostraTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(96, 72);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(121, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "TeostraTimer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TeostraTimer
+{
+    public partial class Form1 : Form
+    {
+        const int basetime = 100;
+        int time = 100; 
+        Timer atimer = new Timer();
+        SoundPlayer simpleSound = new SoundPlayer(Properties.Resources.beep);
+
+        public Form1()        {
+            InitializeComponent();
+            textBox1.Text = time.ToString();
+            atimer.Tick += new EventHandler(OnTimedEvent);
+            atimer.Interval = 1000;
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+            this.Icon = Properties.Resources.Icon1;
+        }
+
+        private void Start()
+        {
+            atimer.Start();
+            button1.Text = "Stop";
+        }
+
+        private void Reset()
+        {
+            atimer.Stop();
+            button1.Text = "Start";
+            time = basetime;
+            textBox1.Text = time.ToString();
+        }
+        private void OnTimedEvent(object sender, EventArgs e)
+        {
+            if (time > 0)
+            {
+                time = time - 1;
+                if (time < 11)
+                    simpleSound.Play();
+                textBox1.Text = time.ToString();
+            }
+            else
+            {
+                atimer.Stop();
+                Reset();
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.NumPad1)
+            {
+                Start();
+            }
+            else if (e.KeyCode == Keys.NumPad2)
+            {
+                Reset();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Start")
+            {
+                Start();
+            }
+            else
+            {
+                Reset();
+            }
+        }
+    }
+}
